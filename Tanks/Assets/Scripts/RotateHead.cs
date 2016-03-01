78cc1cc6afafb58e9cd895897ab4d84b69c95237
etay129 <evangeline129@hotmail.com>
2016-03-01 04:20:07 -0800

    rotating gun head

---
+++ Tanks/Assets/Scripts/RotateHead.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateHead : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	private Vector3 initialAngle;
+	private Vector3 curAngles;
+
+	void Start() {
+		initialAngle = transform.localEulerAngles;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("z")) {
+			curAngles.y -= rotationSpeed * Time.deltaTime;
+			//transform.Rotate (new Vector3 (0, -rotationSpeed, 0) * Time.deltaTime);
+	
+		}
+		if (Input.GetKey ("c")) {
+			curAngles.y += rotationSpeed * Time.deltaTime;
+			//transform.Rotate (new Vector3 (0, rotationSpeed, 0) * Time.deltaTime);
+		}
+
+		curAngles.y = Mathf.Clamp(curAngles.y, -90, 90); 
+		transform.localEulerAngles = initialAngle + curAngles; 
+	
+	}
+}
